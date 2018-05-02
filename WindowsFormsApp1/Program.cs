6ebc8f9527ee7afd6a3b6c70a054079a4238890f
Harshit <test@test.com>
2018-05-02 22:26:12 +0530

    test changes

--- WindowsFormsApp1/Program.cs
+++ WindowsFormsApp1/Program.cs
@@ -14,6 +14,7 @@ namespace WindowsFormsApp1
         [STAThread]
         static void Main()
         {
+            var a = "sasa";
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
